50dea1dc1aeddf490d48088032fd59fa365ac009
leonardo <leonardo_trifiletti@yahoo.it>
2023-11-16 17:35:17 +0100

    es 2 complete

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* 1. Crea una funzione safeParseJSON che prende una stringa e tenta di analizzarla come JSON. Se l'analisi fallisce,la funzione dovrebbe restituire 
 null invece di generare un errore. Inoltre, la funzione dovrebbe stampare in console se l'analisi è stata un successo o meno.*/
 
-let stringa = '["Ciao", "Buonasera"]';
+/*let stringa = '["Ciao", "Buonasera"]';
 
 const safeParseJSON = (stringa) => {
   try {
@@ -12,6 +12,18 @@ const safeParseJSON = (stringa) => {
     console.log("Male");
     return null;
   }
+};*/
+
+//safeParseJSON(stringa);//
+
+/* 2. Crea una funzione chiamata logElements che prende un array e stampa in console ogni elemento utilizzando il metodo .forEach().*/
+
+const array = [1, 2, 3, 4, 5];
+
+const logElements = (array) => {
+  array.forEach((elem) => {
+    console.log(elem);
+  });
 };
 
-safeParseJSON(stringa);
+logElements(array);
